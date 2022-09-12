9bd614288321175dfa6bb84ebb764e9bd18db9bf
boredcod <84103200+boredcod@users.noreply.github.com>
2022-09-11 21:00:25 -0400

    initial commit

---
+++ app/src/main/java/com/example/assignment1/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.assignment1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        TextView text = (TextView) findViewById(R.id.textView);
+        text.setText("Hello World!");
+    }
+}
